14112d0643aac3d053a8e7f653b6b0b460ada8cb
MahmoudAladwy <mahmoudaladwy774@gmail.com>
2026-06-11 11:33:35 +0300

    - adding splash screen

---
+++ lib/Core/routing/tempCodeRunnerFile.dart
@@ -0,0 +1,21 @@
+ // GoRoute(
+      //   path: kOnBoardingScreen,
+      //   builder: (context, state) {
+      //     return const OnBoardingScreen();
+      //   },
+      // ),
+      // GoRoute(
+      //   path: kLoginScreen,
+      //   builder: (context, state) {
+      //      return const LoginScreen();
+      //   },
+      // ),
+    
+      // GoRoute(
+      //   path: kSignUpScreen,
+      //   builder: (context, state) {
+        
+         
+      //   return const SignUpScreen();
+      //   },
+      // ),
